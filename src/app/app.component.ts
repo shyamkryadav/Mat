3a6d4c9a438999a1c83c4ab854c5da629894c9dc
shyamkryadav <shyamkryadav345@gmail.com>
2023-08-10 02:03:46 +0545

    install all material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-root',
